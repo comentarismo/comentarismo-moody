73913e2064920e70d9ca04c3daabee2f46bcb657
Thomas Modeneis <thomas.modeneis@gmail.com>
2016-05-26 09:32:55 +0200

    #11

--- processchecker.sh
+++ processchecker.sh
@@ -3,14 +3,6 @@
 # given script runs continuously every 5m checking for downtime
 # */5 * * * * /opt/gocode/src/comentarismo-moody/main
 
-REDIS_HOST=localhost
-REDIS_PORT=6379
-
-db=ns351151.ip-91-121-75.eu:28015
-
-export db
-export REDIS_HOST
-export REDIS_PORT
 
 cd /opt/gocode/src/comentarismo-moody
 for PROCESS in main
@@ -24,6 +16,6 @@ flock --exclusive --nonblock 221 ||
 }
 echo $$>&221
 echo "$PROCESS is not running, I am going to start an instance!!!";
-nohup ./$PROCESS 2>&1 > /tmp/comentarismo-moody_$PROCESS.log &.
+REDIS_HOST=localhost REDIS_PORT=6379 db=ns351151.ip-91-121-75.eu:28015 nohup ./$PROCESS 2>&1 > /tmp/comentarismo-moody_$PROCESS.log &.
 
 done
